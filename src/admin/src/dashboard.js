79bb9a10fcab78ba42decc4fa8b9d5d3dc9f10ec
馒头饭 <mhjlw@126.com>
2021-11-28 17:12:19 +0800

    :new:清空数据

--- src/admin/src/dashboard.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { Card, CardContent, CardHeader } from '@material-ui/core'
-
-export default () => (
-  <Card>
-    <CardHeader title="Welcome to the administration" />
-    <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-  </Card>
-)
