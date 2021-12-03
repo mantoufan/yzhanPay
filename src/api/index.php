cbfacbb6f5ac1d9e6d885644ea365347354b4d1f
馒头饭 <mhjlw@126.com>
2021-12-03 15:41:08 +0800

    :new:update Access-Control-Allow

--- src/api/index.php
+++ src/api/index.php
@@ -2,6 +2,14 @@
 require __DIR__ . '/vendor/autoload.php';
 $CONFIG = include './config.php';
 $router = new \Bramus\Router\Router();
+
+// 设置允许其他域名访问
+header('Access-Control-Allow-Origin:*');
+// 设置允许的响应类型 
+header('Access-Control-Allow-Methods:POST, GET'); 
+// 设置允许的响应头 
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+
 $router->match('POST', 'authenticate', function() {
 	$_name = $_POST['username'];
 	$_password = $_POST['password'];
--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const PATH_API = '/api/?'
+export const PATH_API = 'https://pay.os120.com/api/?'
