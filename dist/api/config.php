d8875286eaef3edbccfd1fac1b25f79ba611832a
馒头饭 <mhjlw@126.com>
2021-12-03 14:44:14 +0800

    :new:update php api

---
+++ dist/api/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	'md5_salt'=>'madfan',
+	'database' => array(
+		'database_type' => 'mysql',
+		'database_name' => 'name',
+		'server' => 'localhost',
+		'prefix' => 'yzhanpay',
+		'username' => 'your_username',
+		'password' => 'your_password'
+	)
+);
+?>
