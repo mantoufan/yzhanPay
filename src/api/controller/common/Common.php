2c6adb145a6bb1b88b4a4c32b08fa04c5ea95d09
root <root@y5.os120.com>
2021-12-16 16:45:23 +0800

    :new:update configService

--- src/api/controller/common/Common.php
+++ src/api/controller/common/Common.php
@@ -1,15 +1,13 @@
 <?php
 namespace controller\common;
 
-use service\ConfigService;
-
 class Common
 {
     protected $table = 'common';
 
     public function __construct()
     {
-        ConfigService::ConfigInit();
+
     }
     public function __destruct()
     {
